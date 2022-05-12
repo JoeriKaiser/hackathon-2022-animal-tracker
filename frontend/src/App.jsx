9d3aa41368b91c8e25670bbe3ba2523f7607a0bf
JoeriKaiser <joeri.kaiser@hotmail.com>
2022-05-12 22:12:14 +0200

    Created api call to database

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,23 @@ import Logo from './components/common/Logo';
 import MainLayout from './components/layouts/MainLayout';
 import SectionLayout from './components/layouts/SectionLayout';
 import StatsLayout from './components/layouts/StatsLayout';
-
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+  const [AnimalsArray, setAnimalsArray] = useState();
+  const getAnimalData = () => {
+    axios
+      .get('http://localhost:5000/api/animals')
+      .then((response) => setAnimalsArray(response.data[0]));
+  };
+  console.log(AnimalsArray);
+
+  useEffect(() => {
+    getAnimalData();
+    console.log(AnimalsArray);
+  }, []);
+
   return (
     <div className='App'>
       <MainLayout />
