5915449830e62fe0d00e4a28995b00863a8f8f08
JoeriKaiser <joeri.kaiser@hotmail.com>
2022-05-12 22:42:56 +0200

    Added router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,35 @@
 import { useState } from 'react';
 import './App.css';
 import Logo from './components/common/Logo';
-import MainLayout from './components/layouts/MainLayout';
-import SectionLayout from './components/layouts/SectionLayout';
-import StatsLayout from './components/layouts/StatsLayout';
+
 import axios from 'axios';
 import { useEffect } from 'react';
+import AnimalContext from './assets/context/AnimalContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-  const [AnimalsArray, setAnimalsArray] = useState();
+  const [animalsArray, setAnimalsArray] = useState();
   const getAnimalData = () => {
     axios
       .get('http://localhost:5000/api/animals')
       .then((response) => setAnimalsArray(response.data[0]));
   };
-  console.log(AnimalsArray);
 
   useEffect(() => {
     getAnimalData();
-    console.log(AnimalsArray);
   }, []);
 
   return (
-    <div className='App'>
-      <MainLayout />
-      <SectionLayout />
-      <StatsLayout />
-    </div>
+    <Router>
+      <div className='App'>
+        <AnimalContext.Provider value={{ animalsArray, setAnimalsArray }}>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+          </Routes>
+        </AnimalContext.Provider>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/components/layouts/SectionLayout.jsx
+++ frontend/src/components/layouts/SectionLayout.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AnimalContext from '../../assets/context/AnimalContext';
 import CatImage from '../common/AnimalImage';
 
 function SectionLayout() {
+  // const { animalsArray } = useContext(AnimalContext);
+  // const nom = animalsArray.nom;
+  // console.log(nom);
   return (
     <div className='section-container'>
       <CatImage />
-      <h2>Cookie</h2>
+      <h2>jj</h2>
     </div>
   );
 }
