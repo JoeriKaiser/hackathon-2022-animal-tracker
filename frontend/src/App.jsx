d6fdbdb7280bdc9dc9f652c84c74e74830babf89
Lucas-jimenez-dev <pro.lucasjimenez@gmail.com#Downpower2302!Lucas-jimenez-devgit config --global user.email pro.lucasjimenez@gmail.comgit config --global user.email pro.lucasjimenez@gmail.com#>
2022-05-13 09:10:51 +0200

    created caroussel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,26 @@ import Logo from './components/common/Logo';
 import MainLayout from './components/layouts/MainLayout';
 import SectionLayout from './components/layouts/SectionLayout';
 import StatsLayout from './components/layouts/StatsLayout';
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import './components/carousel.css';
 
 function App() {
   return (
     <div className='App'>
       <MainLayout />
       <SectionLayout />
-      <StatsLayout />
+      <Carousel>
+            <StatsLayout />
+            <StatsLayout />
+            <StatsLayout />
+            <StatsLayout />
+            </Carousel>
     </div>
+    
   );
 }
+
+
 
 export default App;
